c695560e62f443011f64652abc9c17f834ca3b97
youyinnn <youyinnn@gmail.com>
2023-01-13 00:56:42 -0500

    jetty dev & deploy

--- src/main/java/org/example/controller/HelloServlet.java
+++ src/main/java/org/example/controller/HelloServlet.java
@@ -1,8 +1,11 @@
 package org.example.controller;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @WebServlet(name = "HelloServlet", value = "/HelloServlet")
